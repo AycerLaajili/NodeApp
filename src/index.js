dcfdaf06a36e6beb71d669f8b033190a6b30992b
Aycer <laajiliaycer6@gmail.com>
2021-04-25 16:36:31 +0100

    init commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const users = [
+    {
+        name: 'Aycer',
+        age: 24
+    },
+    {
+        name: 'Wassim',
+        age: 24
+    },
+    {
+        name: 'Wael',
+        age: 26
+    }
+
+
+]
 
 app.get('/hello', (req, res) => {
     return res.json({ message: 'Hello Node JS !' })
 })
 
-app.get('/bonjour', (req, res) => {
-    return res.json({ message: 'Bonjour Node JS !' })
+app.get('/users', (req, res) => {
+    return res.json(users)
+})
+app.post('/user', (req, res) => {
+    const user = req.body
+    users.push(user)
+    return res.json(user)
 })
 
 app.listen(3000, () => {
